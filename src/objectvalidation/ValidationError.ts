72fad602ca91a31b9383c8522e93bd23d4b797f5
gr33nbl00d <florian.roettges@googlemail.com>
2019-10-15 17:41:12 +0200

    added convenience methods

--- src/objectvalidation/ValidationError.ts
+++ src/objectvalidation/ValidationError.ts
@@ -1,20 +1,47 @@
 import {ValidationRule} from "./ValidationRule";
+import {TextTemplateProcessor} from "..";
 
 export class ValidationRuleError {
-    validationRule:ValidationRule;
+    private validationRule: ValidationRule;
 
     constructor(validationRule: ValidationRule) {
         this.validationRule = validationRule;
     }
+
+    getErrorText(textTemplateProcessor: TextTemplateProcessor) {
+        return this.validationRule.getErrorText(textTemplateProcessor);
+    }
+
+    getValidationRule(): ValidationRule {
+        return this.validationRule;
+    }
+
 }
 
 export class ValidatorError {
-    metadata: any;
-    validationRuleErrors:Array<ValidationRuleError>=[];
+    private metadata: any;
+    private validationRuleErrors: Array<ValidationRuleError> = [];
 
     constructor(metadata: any, validationRuleErrors: Array<ValidationRuleError>) {
         this.metadata = metadata;
         this.validationRuleErrors = validationRuleErrors;
 
     }
+
+    getRuleErrors(): Array<ValidationRuleError> {
+        return this.validationRuleErrors;
+    }
+
+    getFirstRuleError(): ValidationRuleError {
+        if (this.validationRuleErrors.length > 0)
+            return this.validationRuleErrors[0];
+        return null;
+    }
+
+    getFirstRuleErrorText(textTemplateProcessor: TextTemplateProcessor): string {
+        let firstRuleError = this.getFirstRuleError();
+        if (firstRuleError)
+            return firstRuleError.getErrorText(textTemplateProcessor);
+    }
+
 }
